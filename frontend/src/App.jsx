c100a48311219df963bd1e22cc562a229bd34cb2
Tram Pham <nguyettram10@gmail.com>
2024-08-06 15:06:00 +0700

    front-end: Added HomeAuthScreen. Refactor folder structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
-import SignupPage from "./pages/SignupPage";
+import HomePage from "./pages/Home/HomePage";
+import LoginPage from "./pages/Login/LoginPage";
+import SignupPage from "./pages/SignUp/SignupPage";
 
 function App() {
 
