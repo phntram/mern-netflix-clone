96f0773639c58ed0b9034b0c9b3bb2c6c5dd82d3
Tram Pham <nguyettram10@gmail.com>
2024-08-10 21:20:41 +0700

    front-end: Added Content Details page

---
+++ frontend/src/utils/dateFunction.js
@@ -0,0 +1,8 @@
+export const formatReleaseDate = (date) => {
+    return new Date(date).toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    });
+};
+
