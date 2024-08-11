fa016431ed13b79cb95660de8926a457e8143ee8
Tram Pham <nguyettram10@gmail.com>
2024-08-11 14:20:16 +0700

    front-end: Added Search page to search movies, tc shows or person

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import { useAuthStore } from './store/authUser';
 import { useEffect } from 'react';
 import { Loader } from 'lucide-react';
 import { WatchPage } from './pages/Details/WatchPage.jsx';
+import { SearchPage } from './pages/Search/SearchPage.jsx';
 
 function App() {
 	const { user, isCheckingAuth, authCheck } = useAuthStore();
@@ -32,7 +33,8 @@ function App() {
 					<Route path="/" element={<HomePage />} />
 					<Route path="/login" element={!user ? <LoginPage /> : <Navigate to={"/"} />} />
 					<Route path="/signup" element={!user ? <SignupPage /> : <Navigate to={"/"} />} />
-					<Route path="/watch/:id" element={user ? <WatchPage /> : <Navigate to={"/"} />} />
+					<Route path="/watch/:id" element={user ? <WatchPage /> : <Navigate to={"/login"} />} />
+					<Route path='/search' element={user ? <SearchPage /> : <Navigate to={"/login"} />} />
 				</Routes>
 			</Router>
 
