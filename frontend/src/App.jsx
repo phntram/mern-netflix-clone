310fc27de9ceaf11f3858602d98aa21d044e4cdf
Tram Pham <nguyettram10@gmail.com>
2024-08-11 20:31:16 +0700

    front-end: Added SearchHistory page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import { useEffect } from 'react';
 import { Loader } from 'lucide-react';
 import { WatchPage } from './pages/Details/WatchPage.jsx';
 import { SearchPage } from './pages/Search/SearchPage.jsx';
+import { SearchHistoryPage } from './pages/Search/SearchHistoryPage.jsx';
 
 function App() {
 	const { user, isCheckingAuth, authCheck } = useAuthStore();
@@ -35,6 +36,7 @@ function App() {
 					<Route path="/signup" element={!user ? <SignupPage /> : <Navigate to={"/"} />} />
 					<Route path="/watch/:id" element={user ? <WatchPage /> : <Navigate to={"/login"} />} />
 					<Route path='/search' element={user ? <SearchPage /> : <Navigate to={"/login"} />} />
+					<Route path='/history' element={user ? <SearchHistoryPage /> : <Navigate to={"/login"} />} />
 				</Routes>
 			</Router>
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -30,7 +30,7 @@ export const Navbar = () => {
                     <Link to={"/"} className="hover:underline" onClick={() => setContentType("tv")}>
                         TV Shows
                     </Link>
-                    <Link to={"/hitory"} className="hover:underline">
+                    <Link to={"/history"} className="hover:underline">
                         Search History
                     </Link>
                 </div>
@@ -52,7 +52,7 @@ export const Navbar = () => {
                 <div className="w-full sm:hidden mt-4 z-50 bg-black border rounded border-gray-800">
                     <Link to={"/"} className="block hover:underline p-2" onClick={toggleMobileMenu}>Movies</Link>
                     <Link to={"/"} className="block hover:underline p-2" onClick={toggleMobileMenu}>TV Shows</Link>
-                    <Link to={"/hitory"} className="block hover:underline p-2" onClick={toggleMobileMenu}>
+                    <Link to={"/history"} className="block hover:underline p-2" onClick={toggleMobileMenu}>
                         Search History
                     </Link>
                 </div>
